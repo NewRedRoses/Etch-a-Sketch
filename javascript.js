8eadd5b55b0ca573f81a5652d2567e4926b7f10e
Mark A <mark001616@gmail.com>
2023-08-03 16:51:44 -0400

    Change container width to fit all 16x16 squres

--- javascript.js
+++ javascript.js
@@ -8,13 +8,19 @@ for (let x = 0; x < NUM_OF_ROWS; x++) {
   for (let y = 0; y < NUM_OF_COLUMNS; y++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("gridItem");
-    gridItem.classList.add(counter);
+    // gridItem.classList.add(counter);
     grid.appendChild(gridItem);
     counter++;
   }
 }
 const element = document.querySelector("#grid");
 element.addEventListener("mouseover", (event) => {
-  // change background to current hovered div
+  // if grid item only  run this... 
+  if (event.target.getAttribute('class') == "gridItem"){
+    console.log("coloring");
+  }
+    // change background to current hovered div
   event.target.classList.add("coloredGridItem");
-});
+  }
+  
+);
