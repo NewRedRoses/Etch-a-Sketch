f70d97a0b5776774f6a229571a80548aee7fa1d9
Mark A <mark001616@gmail.com>
2023-08-03 20:04:02 -0400

    Fix coloring all boxes instantly

--- javascript.js
+++ javascript.js
@@ -8,19 +8,14 @@ for (let x = 0; x < NUM_OF_ROWS; x++) {
   for (let y = 0; y < NUM_OF_COLUMNS; y++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("gridItem");
-    // gridItem.classList.add(counter);
     grid.appendChild(gridItem);
     counter++;
   }
 }
 const element = document.querySelector("#grid");
 element.addEventListener("mouseover", (event) => {
-  // if grid item only  run this... 
-  if (event.target.getAttribute('class') == "gridItem"){
-    console.log("coloring");
+  // if grid item only  run this...
+  if (event.target.getAttribute("class") == "gridItem") {
+    event.target.classList.add("coloredGridItem");
   }
-    // change background to current hovered div
-  event.target.classList.add("coloredGridItem");
-  }
-  
-);
+});
