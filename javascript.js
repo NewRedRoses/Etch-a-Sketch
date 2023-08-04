2a8f98890c48c8a9155dafbe756473e84479e0db
Mark A <mark001616@gmail.com>
2023-08-04 04:00:19 -0400

    Add functionality to add specific shows

--- javascript.js
+++ javascript.js
@@ -1,21 +1,56 @@
-const grid = document.querySelector("#grid");
+const button = document.querySelector("#rowChanger");
 
-const NUM_OF_ROWS = 16;
-const NUM_OF_COLUMNS = 16;
+let userInput = undefined;
 
-let counter = 0;
-for (let x = 0; x < NUM_OF_ROWS; x++) {
-  for (let y = 0; y < NUM_OF_COLUMNS; y++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("gridItem");
-    grid.appendChild(gridItem);
-    counter++;
+button.addEventListener("click", (event) => {
+  userInput = prompt(
+    "Please type in how many rows you want.\nThe limit is 30."
+  );
+  deleteCurrentGrid();
+  createGrid(userInput, userInput);
+});
+
+if (userInput != undefined) {
+  console.log("user input is no longer undefined");
+  createGrid(userInput, userInput);
+} else {
+  console.log("user input is undefined");
+  createGrid(16, 16);
+}
+colorCellGrid();
+
+function colorCellGrid() {
+  const element = document.querySelector("#grid");
+  element.addEventListener("mouseover", (event) => {
+    // if grid item is hovered over  add special class...
+    if (event.target.getAttribute("class") == "gridItem") {
+      console.log("Coloring");
+      event.target.classList.add("coloredGridItem");
+    }
+  });
+}
+
+function createGrid(NUM_OF_ROWS, NUM_OF_COLUMNS) {
+  let grid = document.createElement("div");
+  const container = document.querySelector(".container");
+
+  grid.setAttribute("id", "grid");
+  container.appendChild(grid);
+
+  const gridSelector = document.querySelector("#grid");
+  let counter = 0;
+  for (let x = 0; x < NUM_OF_ROWS; x++) {
+    for (let y = 0; y < NUM_OF_COLUMNS; y++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("gridItem");
+      gridSelector.appendChild(gridItem);
+      counter++;
+    }
   }
 }
-const element = document.querySelector("#grid");
-element.addEventListener("mouseover", (event) => {
-  // if grid item only  run this...
-  if (event.target.getAttribute("class") == "gridItem") {
-    event.target.classList.add("coloredGridItem");
-  }
-});
+
+function deleteCurrentGrid() {
+  const allGridItems = document
+    .querySelectorAll(".gridItem")
+    .forEach((e) => e.remove());
+}
